81afe0f89f84a861a38d5730481ca426b32e668b
yaseen <yaseen123bcbx@gmail.com>
2023-03-22 16:00:58 -0400

    FComment

---
+++ app/src/main/java/com/example/firebasecloud/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.firebasecloud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        save.setOnClickListener {
+            var name = PersonID.text.toString()
+            var id = PersonName.text.toString()
+            var age = PersonAge.text.toString()
+            val person = hashMapOf(
+                "name" to name,
+                "id" to id,
+                "age" to age
+            )
+            db.collection("Persons")
+                .add(person)
+                .addOnSuccessListener { documentReference ->
+                    Toast.makeText(applicationContext,"${documentReference.id}", Toast.LENGTH_SHORT).show()
+
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(applicationContext,"$e", Toast.LENGTH_SHORT).show()
+
+
+                }
+        }
+
+    }
+}
