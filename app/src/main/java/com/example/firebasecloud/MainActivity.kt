17eabcf93f3c7694a0b9b2d17a627c016aa6b6be
yaseen <yaseen123bcbx@gmail.com>
2023-03-22 16:05:44 -0400

    SComment

--- app/src/main/java/com/example/firebasecloud/MainActivity.kt
+++ app/src/main/java/com/example/firebasecloud/MainActivity.kt
@@ -8,7 +8,7 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+   //save to firebase
     val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
